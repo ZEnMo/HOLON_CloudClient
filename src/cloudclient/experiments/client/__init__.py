e705ed39d3155e9b3568c04563c1c7d9587e7ede
thesethtruth <s.w.p.vanwieringen@gmail.com>
2022-12-14 19:43:19 +0100

    this might do something for the urllib error

--- src/cloudclient/experiments/client/__init__.py
+++ src/cloudclient/experiments/client/__init__.py
@@ -3,6 +3,47 @@ from cloudclient.experiments.config import config, CONFIG_PATH
 from cloudclient.experiments.client.inputs import Inputs
 import importlib
 import sys
+import urllib
+from urllib.request import Request
+from urllib.error import HTTPError
+from anylogiccloudclient.client.cloud_error import CloudError
+import json
+
+
+class HttpClient:
+    def __init__(self, root_url, api_key):
+        self.root_url = root_url
+        self.api_key = api_key
+
+    def api_request(self, url, method="GET", body=None):
+        try:
+            request = Request(
+                self.root_url + urllib.parse.quote(url),
+                method=method,
+                unverifiable=False,
+            )
+            if body is not None:
+                request.data = json.dumps(body).encode("utf-8")
+            self._merge_default_headers(request)
+            response = urllib.request.urlopen(request)
+        except HTTPError as err:
+            raise CloudError.from_json(json.loads(err.read()))
+        return json.loads(response.read())
+
+    def request(self, url, method="GET", body=None):
+        try:
+            request = Request(self.root_url + url, method=method, unverifiable=False)
+            if body is not None:
+                request.data = body
+            self._merge_default_headers(request)
+            response = urllib.request.urlopen(request)
+        except HTTPError as err:
+            raise CloudError.from_json(json.loads(err.read()))
+        return json.loads(response.read())
+
+    def _merge_default_headers(self, request):
+        request.add_header("Authorization", self.api_key)
+        request.add_header("Content-Type", "application/json")
 
 
 class Client(Inputs):
@@ -10,7 +51,11 @@ class Client(Inputs):
         self.client = CloudClient(
             config["anylogic_cloud"]["api_key"], config["anylogic_cloud"]["url"]
         )
-        
+        # THIS IS NASTY! Overwrite the HTTP client used in the cloudclient module
+        self.client._http_client = HttpClient(
+            self.client.root_url, config["anylogic_cloud"]["api_key"]
+        )
+
         # the order matters!
         self.experiment = experiment
         # init the data model if required:
