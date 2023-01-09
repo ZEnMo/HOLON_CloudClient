a2e81631394fb0450523dc9d83b28f56c7ff6313
GillisHommen <98752265+GillisHommen@users.noreply.github.com>
2023-01-09 18:03:41 +0100

    Integrated ETM-upscaling in cloudclient (crudely)

--- src/cloudclient/experiments/__init__.py
+++ src/cloudclient/experiments/__init__.py
@@ -66,12 +66,12 @@ def start_experiment(settings):
         api_experiment.runSimulation()
     print("\nDuration: ", api_experiment.duration_s, " seconds\n")
     # return api_experiment
-    calculateAreaCosts(api_experiment)
+    calculateAllKPIs(api_experiment)
 
 
-def calculateAreaCosts(api_experiment):
-    grid_connection_config_json = api_experiment.client.inputs.get_input(
-        "P grid connection config JSON"
+def calculateAllKPIs(api_experiment):
+    grid_connection_config = json.loads(
+        api_experiment.client.inputs.get_input("P grid connection config JSON")
     )
     gridnode_config = json.loads(
         api_experiment.client.inputs.get_input("P grid node config JSON")
@@ -79,9 +79,6 @@ def calculateAreaCosts(api_experiment):
 
     experiment_outputs = api_experiment.outcomes.get("APIOutputTotalCostData")
     hourly_curves = api_experiment.outcomes.get("APIOutputHourlyCurvesData")
-    # print('calculate area-costs holon_config', api_experiment.client.inputs.get_input('P grid connection config JSON'))
-    # print('calculate area-costs holon_output', api_experiment.outcomes.get('APIOutputTotalCostData'))
-
     # pd.set_option("display.max_rows", 50000)
     # pd.set_option("display.expand_frame_repr", True)
     # pd.set_option('display.width', 1000)
@@ -91,26 +88,26 @@ def calculateAreaCosts(api_experiment):
     # print("real inputs ", real_inputs)
     # print("offered inputs ", experiment_inputs)
 
-    print(COSTS_SCENARIO_ID)
-    print(ETM_CONFIG_PATH)
-    print(ETM_CONFIG_FILE_COSTS)
-    etm_service.retrieve_results(
-        COSTS_SCENARIO_ID, ETM_CONFIG_PATH, ETM_CONFIG_FILE_COSTS
-    )
+    # print(COSTS_SCENARIO_ID)
+    # print(ETM_CONFIG_PATH)
+    # print(ETM_CONFIG_FILE_COSTS)
+    # etm_service.retrieve_results(
+    #     COSTS_SCENARIO_ID, ETM_CONFIG_PATH, ETM_CONFIG_FILE_COSTS
+    # )
 
     # experiment_inputs2 = experiment_inputs.replace("null", "None")
-    grid_connection_config = json.loads(grid_connection_config_json)
+
     # print("offered inputs2 ", grid_connection_config)
     # print("Hourly curves type:", type(hourly_curves[0]))
     print("\nExperiment output categories:", experiment_outputs[0].keys())
     # print("Hourly curves categories:", hourly_curves[0].keys())
     # result = calculate_total_costs(etm_output(), holon_config(), holon_output())
-    result = calculate_total_costs_split(
+    areaCosts_kEur = calculate_total_costs_split(
         etm_output(), grid_connection_config, experiment_outputs[0], hourly_curves[0]
     )
-    print("Total area costs: ", result, " kEur  ")
+    print("Total area costs: ", areaCosts_kEur, " kEur  ")
 
-    kpiresults = calculate_holon_kpis(
+    area_KPI_results = calculate_holon_kpis(
         total_cost_data=experiment_outputs[0],
         hourly_curves=hourly_curves[0],
         etm_data=etm_service.retrieve_results(
@@ -121,4 +118,60 @@ def calculateAreaCosts(api_experiment):
         ),
     )
     print("gebieds-kpi's: ")
-    print(kpiresults)
+    print(area_KPI_results)
+
+    # try ETM upscaling to national level. Need to
+    etm_slider_settings = {}
+    slider_etm_key = (
+        "share_of_electric_trucks"  # "transport_truck_using_electricity_share"
+    )
+    slider_value = 2
+    etm_slider_settings.update({slider_etm_key: slider_value})
+    print("ETM slider settings:", etm_slider_settings)
+    holon_output = {}
+    try:
+        holon_output = {
+            "totalEHGVHourlyChargingProfile_kWh": hourly_curves[0][
+                "totalEHGVHourlyChargingProfile_kWh"
+            ]
+        }
+    except KeyError:
+        print("pepe.etm_kpi_holon_output: Resolving to empty values for this key!")
+
+    # print("holon_output: ", holon_output)  # , " and size: ", size(holon_output))
+
+    etm_upscale_scenario_id = etm_service.scale_copy_and_send(
+        COSTS_SCENARIO_ID,
+        etm_slider_settings | holon_output,
+        ETM_CONFIG_PATH,
+        ETM_CONFIG_FILE_SCALING,
+    )
+    etm_upscale_results = etm_service.retrieve_results(
+        etm_upscale_scenario_id, ETM_CONFIG_PATH, ETM_CONFIG_FILE_GET_KPIS
+    )
+
+    print("ETM upscale results: ", etm_upscale_results)
+
+    results = {
+        "national": {
+            "netload": round(etm_upscale_results["national_kpi_network_load"], 1),
+            "costs": round(
+                etm_upscale_results["national_total_costs"], -8
+            ),  # reduce significance
+            # ETM returns factor instead of percentage for sustainability
+            "sustainability": round(
+                100 * etm_upscale_results["national_renewability"], 1
+            ),
+            "self_sufficiency": round(
+                etm_upscale_results["national_kpi_self_sufficiency"], 1
+            ),
+        }
+    }
+    # ,
+    #     "local": {
+    #         "costs": round(self.total_costs, -3),  # reduce significance
+    #         area_KPI_results,
+    #     },
+    # }
+
+    print(results)
